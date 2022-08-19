1a8eac43e5373e1dfc778e1ae6a3883032c0553b
Herbert Lange <lange@ids-mannheim.de>
2022-08-19 13:42:07 +0200

    add hashed ntuple storage layout extension

---
+++ src/main/java/de/ids_mannheim/lza/ocfl/mini/HashedNtupleStorageExtension.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.ids_mannheim.lza.ocfl.mini;
+
+/**
+ * Class representing the Hashed N-tuple Storage Layout
+ * https://github.com/OCFL/extensions/blob/main/docs/0004-hashed-n-tuple-storage-layout.md
+ * @author Herbert Lange <lange@ids-mannheim.de>
+ */
+public class HashedNtupleStorageExtension implements StorageExtension {
+
+    // The digest algorithm used for hashing
+    DigestAlgorithm digestAlgorithm;
+    // Number of characters in a tuple
+    int tupleSize;
+    // Number of tuples to be used in path
+    int tupleCount;
+    
+    /**
+     * Constructor for the Hashed N-tuple Storage Layout extension
+     * @param tupleSize the number of characters in a tuple
+     * @param tupleCount the number of tuples
+     * @param digestAlgorithm the digest algorithm
+     */
+    public HashedNtupleStorageExtension(int tupleSize,int tupleCount, DigestAlgorithm digestAlgorithm) {
+        this.digestAlgorithm = digestAlgorithm;
+        this.tupleSize = tupleSize;
+        this.tupleCount = tupleCount;        
+    }
+
+    
+    @Override
+    public String getObjectPath(String id) {
+        StringBuilder path = new StringBuilder();
+        String hash = digestAlgorithm.hashString(id);
+        for (int i = 0; i < tupleCount; i++) {
+            path.append(hash.substring(i*tupleSize, i*tupleSize + tupleSize));
+            path.append("/");
+        }
+        path.append(hash);
+        return path.toString();
+    }
+    
+}
